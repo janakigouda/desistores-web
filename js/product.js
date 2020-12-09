36e741dd75f2991b59eda0c80de68435452d18b5
janakigouda <janakikangalgouda@gmail.com>
2020-12-09 16:32:11 +0530

    [9th dec]

--- js/product.js
+++ js/product.js
@@ -4,3 +4,23 @@ $(document).ready(function(){
     $(this).toggleClass("expand").siblings().removeClass('expand');
   });
 });
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $(".wrapper .s-product .row").addClass("stick");
+    } else {
+      $(".wrapper .s-product .row").removeClass("stick");
+    }
+  });
+});
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {
+      $(".wrapper .s-product").addClass("abc");
+    } else {
+      $(".wrapper .s-product").removeClass("abc");
+    }
+  });
+});
